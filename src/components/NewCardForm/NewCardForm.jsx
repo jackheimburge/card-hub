1c96da554f8287bfc5bb0d057db330351f65b874
centipedejam <jheimburge@gmail.com>
2023-10-23 14:13:29 -0700

    add carousel to CardDetailPage

--- src/components/NewCardForm/NewCardForm.jsx
+++ src/components/NewCardForm/NewCardForm.jsx
@@ -4,6 +4,7 @@ import * as cardsAPI from '../../utilities/cards-api';
 import * as imagesAPI from '../../utilities/images-api';
 
 export default function NewCardForm({ allCards, setAllCards }) {
+    const [isUploading, setIsUploading] = useState(false);
     const [card, setCard] = useState({
         player: '',
         year: '',
@@ -17,6 +18,7 @@ export default function NewCardForm({ allCards, setAllCards }) {
 
     async function handleSubmit(e) {
         e.preventDefault();
+        setIsUploading(true);
         const formData = new FormData();
         for (let i = 0; i < fileInputRef.current.files.length; i++) {  // Create a new FormData object and append the selected images
             formData.append('images', fileInputRef.current.files[i]);
@@ -38,6 +40,7 @@ export default function NewCardForm({ allCards, setAllCards }) {
             images: []
         });
         fileInputRef.current.value = ''; // Clear the file input
+        setIsUploading(false);
     }
 
     const handleChange = (e) => { // Update the state of the new card being created
@@ -51,20 +54,20 @@ export default function NewCardForm({ allCards, setAllCards }) {
         <div>
             <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <label htmlFor="player">Player</label>
-                <input name="player" value={card.player} id="player" onChange={handleChange} />
+                <input required name="player" value={card.player} id="player" onChange={handleChange} />
                 <label htmlFor="year">Year</label>
-                <input name="year" id="year" value={card.year} onChange={handleChange} />
+                <input required name="year" id="year" value={card.year} onChange={handleChange} />
                 <label htmlFor="title">Title</label>
-                <input name="title" id="title" value={card.title} onChange={handleChange} />
+                <input required name="title" id="title" value={card.title} onChange={handleChange} />
                 <label htmlFor="price">Price</label>
-                <input name="price" id="price" value={card.price} onChange={handleChange} />
+                <input required name="price" id="price" value={card.price} onChange={handleChange} />
                 <label htmlFor="image">Image</label>
-                <input type="file" name="images" ref={fileInputRef} id="images" multiple />
+                <input required type="file" name="images" ref={fileInputRef} id="images" multiple />
                 <label htmlFor="category">Sport</label>
-                <input name="category" id="category" value={card.category} onChange={handleChange} />
+                <input required name="category" id="category" value={card.category} onChange={handleChange} />
                 <label htmlFor="description">Condition/Other Details</label>
-                <input name="description" id="description" value={card.description} onChange={handleChange} />
-                <button type="submit">Submit</button>
+                <input required name="description" id="description" value={card.description} onChange={handleChange} />
+                <button disabled={isUploading} type="submit">{isUploading ? 'Loading..' : 'Submit'}</button>
             </form>
         </div>
     );
--- src/pages/CardDetailPage/CardDetailPage.jsx
+++ src/pages/CardDetailPage/CardDetailPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import * as cardsAPI from '../../utilities/cards-api';
 import { useParams } from 'react-router-dom';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css'
 import './CardDetailPage.css';
 
 export default function CardDetailPage() {
@@ -13,14 +15,17 @@ export default function CardDetailPage() {
             setCard(returnedCard);
         }
         getCard();
-    }, []);
+    }, [id]);
     return (
         <div className='CardDetailPage'>
             {card ? (
-                <>
-                    <h1>The card is {card.player}</h1>
-                    <img src={card.images[0]} alt="" />
-                </>
+                <div className='carousel'>
+                    <Splide aria-label="My Favorite Images">
+                        {card.images.map((image, idx) => <SplideSlide>
+                            <img key={idx} src={image} alt={`${idx + 1}`} />
+                        </SplideSlide>)}
+                    </Splide>
+                </div>
             ) : (<p>Loading...</p>)
             }
         </div>
