e2b0af8603bd77bdb0c647d78f014a9ba4e100b1
centipedejam <jheimburge@gmail.com>
2023-10-26 10:00:57 -0700

    save filtered cards and setcart with those cards

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,7 @@
+export default function cartItem({ card }) {
+    return (
+        <div>
+            <h1>{card.price}</h1>
+        </div>
+    );
+}
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,20 +1,23 @@
 import { useEffect } from 'react';
 import * as ordersAPI from '../../utilities/orders-api';
+import CartItem from '../../components/CartItem/CartItem';
 import './CartPage.css';
 
 export default function CartPage({ setCart, cart, allCards }) {
     useEffect(function () {
         async function getCart() {
             const cart = await ordersAPI.getCart();
-            console.log(cart)
-            setCart(allCards.filter((card) => cart.cards.includes(card._id)));
+            console.log('before', cart)
+            const cartItems = allCards.filter((card) => cart.cards.includes(card._id));
+            setCart(cartItems)
+            console.log('after', cartItems);
         }
         getCart();
-    }, [setCart]);
+    }, [setCart, allCards]);
 
     return (
         (Array.isArray(cart) && cart.length > 0) ? (
-            cart.map((card, idx) => <p key={idx}>{card.player} ${card.price}</p>)
+            cart.map((card) => <CartItem key={card._id} card={card} />)
         ) : <h1>Cart is Empty</h1>
     );
 }
